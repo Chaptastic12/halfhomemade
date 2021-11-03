b7d2759418bca75ba1e70313db6cd4eacd7067ea
Chaptastic12 <bchapman51@gmail.com>
2021-11-03 14:18:09 -0500

    Minor color changes
    Tweaked colors a bit in the Nav as well as AboutDiv

--- src/Shared/components/NavBar/TopNav.js
+++ src/Shared/components/NavBar/TopNav.js
@@ -4,7 +4,7 @@ const TopNav = props =>{
 
         return(
             <div className='TopNav d-flex justify-content-center align-items-center text-center'>
-                <span>Use discount code <strong>NEWBOOK2021</strong> for free shipping!</span>
+                <span>Use discount code <strong>NEWBOOK2021</strong> for free shipping! <i className="fas fa-birthday-cake"/></span>
             </div>)    
 }
 
---
+++ src/pages/RecipePage/RecipePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const RecipePage = props =>{
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default RecipePage;
