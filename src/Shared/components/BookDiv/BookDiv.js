f433f7790b34b2f796a0f0c2aa9ec4dbefa2cf90
Chaptastic12 <bchapman51@gmail.com>
2022-01-29 19:15:47 -0600

    Update to the navbar title as well as the display for bookdiv

--- src/Shared/components/BookDiv/BookDiv.js
+++ src/Shared/components/BookDiv/BookDiv.js
@@ -18,6 +18,27 @@ const BookDiv = props =>{
     const loadedBookCover = useProgressiveImage(BookCover);
     const loadedDivBackground = useProgressiveImage(DivBackground)
 
+    return (
+        <div className='d-flex justify-content-center align-items-center BookDiv3' style={{backgroundImage: 'URL(' + loadedDivBackground || null + ')'}} >
+            <Row>
+                <Col xs={6}>            
+                    <div className="header2 d-flex justify-content-center align-items-end" style={{backgroundImage: 'URL(' + loadedBookCover || null + ')'}}>
+                        <div className='BookDiv-BookText3  text-center'>
+                            <h1>Check out our new book!</h1>
+                            <p>Bringing the best recipes from around the world to one book, right in your kitchen.<span className='d-none d-md-block'>
+                            With recipes from Japan to America, you're sure to find something to enjoy for even the pickiest of eaters.</span></p>
+                            <span className='BookDiv-Buttons'>
+                                <Button className='BookDiv-Button' variant='outline-dark' as={NavLink} to='/shop/'>Buy the book </Button>
+                                <Button className='BookDiv-Button' variant='outline-dark' as={NavLink} to ='/recipes/all'>View Recipes</Button>
+                            </span>
+                            <p className='BookDiv-LearnMore'>Learn more <i className="far fa-arrow-alt-circle-right" /></p>
+                        </div>
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    )
+
     if(test){
         return (
             <div className='BookDiv2' style={{backgroundImage: 'URL(' + loadedDivBackground || null + ')'}}>
