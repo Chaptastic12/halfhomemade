22f964f11a2ee0be2ae27e7879ca3c512427f901
Chaptastic12 <bchapman51@gmail.com>
2022-03-18 10:50:56 -0500

    Added component that fades in components on scroll
    App used to just hard load everything, now it feels more dynamic as parts of the page will load in on scroll

--- src/Shared/components/UI Elements/FlippingTextIcon/FlippingTextIcon.js
+++ src/Shared/components/UI Elements/FlippingTextIcon/FlippingTextIcon.js
@@ -1,27 +1,35 @@
 
-import { Row, Col, Container } from 'react-bootstrap';
+import { Row, Col, Container, Fade } from 'react-bootstrap';
 import { v4 as uuid } from 'uuid';
 
+import FadeInSection from '../FadeInSection/FadeInSection';
 import './FlippingTextIcon.css';
 
 const FlippingTextIcon = props =>{
 
     let output = props.items.map(item => {
-        return  <Row key={uuid()} className='FlippingTextIcon'>
-                    <Col className='d-none d-sm-block' xs={item.iconLeft ? {order: 'first'} : {order: 'last'}}>
-                        <div className='Box box-shadow-small'>
-                            {item.icon}
-                        </div>
-                    </Col>
-                    <Col xs={item.iconLeft ? {order: 'last'} : {order: 'first'}}>
-                        <div className='Box'>
-                            <div>
-                                { item.subTitle !== null && <p className='SubTitle'>{item.subTitle}</p> }
-                                <p>{item.desc}</p>
-                            </div>
-                        </div>
-                    </Col>
-                </Row>
+        return (
+          <FadeInSection>
+            <Row key={uuid()} className="FlippingTextIcon">
+              <Col
+                className="d-none d-sm-block"
+                xs={item.iconLeft ? { order: "first" } : { order: "last" }}
+              >
+                <div className="Box box-shadow-small">{item.icon}</div>
+              </Col>
+              <Col xs={item.iconLeft ? { order: "last" } : { order: "first" }}>
+                <div className="Box">
+                  <div>
+                    {item.subTitle !== null && (
+                      <p className="SubTitle">{item.subTitle}</p>
+                    )}
+                    <p>{item.desc}</p>
+                  </div>
+                </div>
+              </Col>
+            </Row>
+          </FadeInSection>
+        );
     })
     
     return <Container> { output } </Container>;
